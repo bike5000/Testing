5baf6d3086f2cc6463c6b52737149b1b5b61d4ff
bike5000 <andrew.hockman@geappliances.com>
2024-06-13 17:12:04 -0400

    something truly cursed

--- C_files/main.c
+++ C_files/main.c
@@ -3,10 +3,26 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+typedef struct {
+    uint16_t f1;
+    uint8_t f2;
+    // padding here
+} Foo_t;
 
 int main()
 {
-    call_thing(thing_1, 2);
-    call_thing(thing_2, 2);
+    Foo_t foo = {
+        .f1 = 0xFEAB,
+        .f2 = 0xCC,
+    };
+    uint8_t * arr = (uint8_t *)(&foo);
+
+    for(int i = 0; i < sizeof(Foo_t); i++) {
+        printf("%x ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
